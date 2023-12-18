0a10232abcea97a77b18f0788af820db2710eb8b
Daniel Cadenas <dcadenas@gmail.com>
2023-12-18 12:24:39 -0300

    Add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('jest').Config} */
 
 export default {
-    modulePathIgnorePatterns: ['./config/'],
+  modulePathIgnorePatterns: ["./config/"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
