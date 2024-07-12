b2b5025d331a5b78aa881bed257732bfc9c53ae4
Daniel Cadenas <dcadenas@gmail.com>
2024-07-12 11:58:58 -0300

    Add news to reserved names, lighter logs

--- src/logger.js
+++ src/logger.js
@@ -5,4 +5,13 @@ export default pino({
   level: config.logLevel,
   timestamp: pino.stdTimeFunctions.isoTime,
   prettyPrint: false, // Ensuring logs are in JSON format, single-line
+  serializers: {
+    req: (req) => ({
+      id: req.id,
+      method: req.method,
+      url: req.url,
+      query: req.query,
+      params: req.params,
+    }),
+  },
 });
